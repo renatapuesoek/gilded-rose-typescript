356d5f816db9b32706d298e6f2ea1ccadb8bd8cd
renatapuesoek <pusokreni@gmail.com>
2022-07-07 11:56:21 +0200

    refactor: abc-123 extract magic strings to constants extract type checking functions introduce early return for Sulfuras

--- src/gilded_rose.ts
+++ src/gilded_rose.ts
@@ -20,6 +20,24 @@ const BACKSTAGE_SECOND_INCREASE = 5;
 
 const SELLIN_EXPIRED = 0;
 
+const ITEMNAME_CHEESE = 'Aged Brie';
+
+const ITEMNAME_BACKSTAGEPASS = 'Backstage passes to a TAFKAL80ETC concert';
+
+const ITEMNAME_SULFURAS = 'Sulfuras, Hand of Ragnaros';
+
+function isCheese(item: Item) {
+  return item.name == ITEMNAME_CHEESE;
+}
+
+function isBackstagePass(item: Item) {
+  return item.name == ITEMNAME_BACKSTAGEPASS;
+}
+
+function isSulfuras(item: Item) {
+  return item.name == ITEMNAME_SULFURAS;
+}
+
 export class Shop {
   items: Item[];
 
@@ -29,16 +47,17 @@ export class Shop {
 
   updateQuality() {
     this.items.forEach(item => {
-      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (isSulfuras(item)) {
+        return
+      }
+      if (!isCheese(item) && !isBackstagePass(item)) {
         if (item.quality > QUALITY_MINIMUM) {
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
-            item.quality -= 1;
-          }
+          item.quality -= 1;
         }
       } else {
         if (item.quality < QUALITY_MAXIMUM) {
           item.quality += 1;
-          if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (isBackstagePass(item)) {
             if (item.sellIn <= BACKSTAGE_FIRST_INCREASE) {
               if (item.quality < QUALITY_MAXIMUM) {
                 item.quality += 1;
@@ -52,16 +71,12 @@ export class Shop {
           }
         }
       }
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
-        item.sellIn -= 1;
-      }
+      item.sellIn -= 1;
       if (item.sellIn < SELLIN_EXPIRED) {
-        if (item.name != 'Aged Brie') {
-          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (!isCheese(item)) {
+          if (!isBackstagePass(item)) {
             if (item.quality > QUALITY_MINIMUM) {
-              if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                item.quality -= 1;
-              }
+              item.quality -= 1;
             }
           } else {
             item.quality = QUALITY_MINIMUM;
